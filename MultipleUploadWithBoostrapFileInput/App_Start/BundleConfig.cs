9e28063c47e7c7fbf6abcfca8779b9f1b7dc99c8
THISPC\TuanBao <hatuanbao.ueh@gmail.com>
2016-12-11 19:54:37 +0700

    add fileinput.js and fileinput.css into project

--- MultipleUploadWithBoostrapFileInput/App_Start/BundleConfig.cs
+++ MultipleUploadWithBoostrapFileInput/App_Start/BundleConfig.cs
@@ -21,11 +21,13 @@ namespace MultipleUploadWithBoostrapFileInput
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/fileinput.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/fileinput.min.css"));
         }
     }
 }
