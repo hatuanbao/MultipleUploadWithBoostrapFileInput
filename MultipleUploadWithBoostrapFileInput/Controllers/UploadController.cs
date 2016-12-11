265b6cbdf364f3e7f76bcef6cb0c965578dbac9d
THISPC\TuanBao <hatuanbao.ueh@gmail.com>
2016-12-11 20:15:12 +0700

    apply fileinput in UploadController

---
+++ MultipleUploadWithBoostrapFileInput/Controllers/UploadController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultipleUploadWithBoostrapFileInput.Controllers
+{
+    public class UploadController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile()
+        {
+            return View("Index");
+        }
+    }
+}
